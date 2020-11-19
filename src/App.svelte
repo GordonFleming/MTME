8ac0d1b785c0ed83d102f1d71cf074d67ceced90
wolfbyne <flemingrgordon@gmail.com>
2020-11-19 23:30:25 +0200

    Content additions and designs

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,32 @@
 <script>
-	let name = 'world';
+    import {fade, slide, scale} from 'svelte/transition';
 </script>
 
-<h1>Hello {name}!</h1>
+<div id="cover" in:slide="{{ duration: 2500 }}">
+
+</div>
+<!-- Main -->
+<div transition:fade="{{ duration: 2000, delay: 1800}}">
+    <center>
+        <a href="https://www.facebook.com/mtme.macktwins.1" target="_blank"><i id="facebook" class="fab fa-facebook-square fa-5x"></i></a>
+        <a href="https://www.youtube.com/channel/UC91OgmMT9nhFcjjDEfmJy-w" target="_blank"><i id="youtube" class="fab fa-youtube fa-5x"></i></a>
+        <a href="https://instagram.com/macktwinsmakeup" target="_blank"><i id="insta" class="fab fa-instagram-square fa-5x"></i></a>
+    </center>
+    
+    <div transition:scale="{{ duration: 2000, delay: 2000}}" class="pad-top">
+        <div class="row center">
+            <div class="col">
+                <img src="portrait_One.jpg" alt="portrait_image" height="1000vh" width="680vw">
+            </div>
+            <div class="col down">
+                <img src="portrait_Two.jpg" alt="portrait_image" height="1000vh" width="680vw">
+            </div>
+        </div>
+    </div> 
+
+    <div class="">
+        <h1>who we are</h1>
+    </div>
+
+    <center><img transition:scale="{{ duration: 2000, delay: 3000}}" src="portrait_Three.jpg" alt="portrait_image" height="800vh" width="1000vw"></center>
+</div>
