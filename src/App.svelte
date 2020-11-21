3476d190d71fa40ceb1aa7daf9e698f2cb4feddb
wolfbyne <flemingrgordon@gmail.com>
2020-11-21 17:57:47 +0200

    Compressed images to webp using lossy compresion at 20

--- src/App.svelte
+++ src/App.svelte
@@ -12,21 +12,34 @@
         <a href="https://www.youtube.com/channel/UC91OgmMT9nhFcjjDEfmJy-w" target="_blank"><i id="youtube" class="fab fa-youtube fa-5x"></i></a>
         <a href="https://instagram.com/macktwinsmakeup" target="_blank"><i id="insta" class="fab fa-instagram-square fa-5x"></i></a>
     </center>
-    
+    <br><br>
+    <h1>who we are</h1>
     <div transition:scale="{{ duration: 2000, delay: 2000}}" class="pad-top">
-        <div class="row center">
-            <div class="col">
-                <img src="portrait_One.webp" alt="portrait_image" height="1000vh" width="680vw">
-            </div>
-            <div class="col down">
-                <img src="portrait_Two.webp" alt="portrait_image" height="1000vh" width="680vw">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <img class="img-fluid" src="portrait_One.webp" alt="portrait_image" height="1000vh" width="680vw">
+                    <p class="pt-4">We eventually left the makeup industry to pursue Jobs In the field in which we studied – Biotechnology.</p>
+                    <p>Yet our passion for makeup never left us and as a side hustle we did some freelancing makeup Jobs.</p>
+                    <p>The Beauty Industry really ignited the passion within us , to push our creativity to the next level and enhance our artistry skill.</p>
+                </div>
+                <div class="col-md-6 col-sm-12">
+                    <p>We are the <strong>Mack Twins</strong>, Chanel & Charnay Mack , professional freelance makeup artists.</p>
+                    <p><strong>Welcome to our Makeup Website</strong></p>
+                    <p class="pb-4">Our makeup Artistry journey started in the year 2013, while studying towards our Bsc degrees. We were approached by @elizabetharden to promote their new lipstick line , and from there got approached by @bobbibrown and worked there for 2 years and then @maccosmeticsafrica for another 2 years and lastly @urbandecaycosmetics .</p>
+                    <img class="img-fluid" src="portrait_Two.webp" alt="portrait_image" height="1000vh" width="680vw">
+                </div>
             </div>
         </div>
-    </div> 
-
-    <div class="">
-        <h1>who we are</h1>
     </div>
 
-    <center><img transition:scale="{{ duration: 2000, delay: 3000}}" src="portrait_Three.webp" alt="portrait_image" height="800vh" width="1000vw"></center>
+    <div class="">
+        <br>
+        <p><strong>All women want to feel beautiful and we want to  ignite that feeling in each and everyone of</strong></p>
+        <h3><strong>YOU</strong></h3>
+        <br>
+        <h2>Mack Twins</h2>
+    </div>
+
+    <center><img class="img-fluid" transition:scale="{{ duration: 2000, delay: 3000}}" src="portrait_Three.webp" alt="portrait_image" height="800vh" width="1000vw"></center>
 </div>
